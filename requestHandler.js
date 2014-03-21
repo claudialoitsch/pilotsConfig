7c26638d4dd72b69437ea4f6012ea8bf25369f27
Claudia Loitsch <claudia.loitsch@tu-dresden.de>
2014-03-21 12:41:33 +0100

    Add licence to all files + license.txt

--- requestHandler.js
+++ requestHandler.js
@@ -1,3 +1,22 @@
+/*!
+    GPII @@pilotsConfig
+
+    Copyright 2014 Technische Universität Dresden (TUD)
+    Copyright 2014 Raising the Floor - International (RtF)
+
+    Licensed under the New BSD license. You may not use this file except in
+    compliance with this License.
+
+    You may obtain a copy of the License at
+    https://github.com/GPII/universal/blob/master/LICENSE.txt
+
+
+    Acknowledgements:
+    The research leading to these results has received funding from 
+    the European Union's Seventh Framework Programme (FP7/2007-2013) 
+    under grant agreement no. 289016.
+*/
+
 var http = require('http');
 var querystring = require("querystring");
 var fs = require('fs');
